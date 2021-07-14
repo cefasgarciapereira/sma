f8e87c58af445aeb947b42a5b0b8cd8dd6b5cc91
Cefas Garcia Pereira <cefasgarcia@gsaeducacional.com.br>
2021-07-13 22:59:27 -0300

    feat(Sentiment): sentiment per topic

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -23,11 +23,39 @@ export default function Home() {
 
     const renderTopics = (n) => {
         var topics = []
+        var topicsSentiments = []
+
         for (let i = 0; i < kTopics; i++) {
             topics.push(model.getTerms(i, n))
         }
+
+        topics.forEach(topic => {
+            var topicSentiment = 0;
+            var n = 1;
+            topic.forEach(t => {
+                documents.forEach(document => {
+                    if (document.includes(t.word)) {
+                        topicSentiment = topicSentiment + sentiment.analyze(document).score
+                        console.log(`${document}: ${sentiment.analyze(document).score}`)
+                    }
+                })
+            })
+            console.log('\n')
+            topicsSentiments.push(topicSentiment)
+        })
+
+        topicsSentiments = topicsSentiments.map(normalize(0, 10))
+        console.log(topicsSentiments)
+        topicsSentiments.forEach((sent, index) => topics[index].sentiment = sent)
+
         return topics
     }
+
+    const normalize = (min, max) => {
+        const delta = max - min;
+        return val => ((val < min ? min : (val > max ? max : val)) - min) / delta;
+    }
+
 
     return (
         <Grid container fullWidth p={4}>
@@ -41,11 +69,10 @@ export default function Home() {
                         renderTopics(5).map((topic, index) => (
                             <Grid item xs={4} key={index}>
                                 <h4>Topic {index}</h4>
+                                <h4>Sentiment {topic.sentiment}</h4>
                                 {topic.map(({ word, prob }) =>
                                     <div key={word}>
-                                        <p>{word}: {prob}</p>
-                                        <p>Sentiment: {sentiment.analyze(word).score}</p>
-                                        <br />
+                                        <p>{word}: {parseFloat(prob).toFixed(2)}</p>
                                     </div>
                                 )}
                             </Grid>
